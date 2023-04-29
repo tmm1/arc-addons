12ac4e9997ce231088adb24897f676252c54ffe2
AuxXxilium <info@auxxxilium.tech>
2023-04-29 21:39:31 +0200

    addons: cpuinfo / codecpatch: update
    Signed-off-by: AuxXxilium <info@visionz.media>

--- codecpatch/install.sh
+++ codecpatch/install.sh
@@ -2,5 +2,9 @@
 
 if [ "${1}" = "late" ]; then
   echo "Installing addon synocodec patch"
-  tar -zxvf /addons/codecpatch.tgz -C /tmpRoot/
+  cp -v /usr/bin/codecpatch.sh /tmpRoot/usr/bin/codecpatch.sh
+  cp -v /usr/lib/systemd/system/codecpatch.service /tmpRoot/usr/lib/systemd/system/codecpatch.service
+  # /lib -> /usr/lib; /etc/systemd/system/multi-user.target.wants/* -> /usr/lib/systemd/system/multi-user.target.wants/*
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/codecpatch.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/codecpatch.service
 fi
