2e8a6bd18e707d3500cdce0eba83226768bf5e8b
AuxXxilium <info@auxxxilium.tech>
2023-05-12 20:02:17 +0200

    addons: add diskdbpatch
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

---
+++ diskdbpatch/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ash
+
+if [ "${1}" = "late" ]; then
+  echo "Creating service to exec DiskDBPatch"
+  cp -v /usr/bin/diskdbpatch.sh /tmpRoot/usr/bin/diskdbpatch.sh
+  DEST="/tmpRoot/lib/systemd/system/diskdbpatch.service"
+  echo "[Unit]"                                                                >${DEST}
+  echo "Description=Enable DiskDBPatch"                                       >>${DEST}
+  echo                                                                        >>${DEST}
+  echo "[Service]"                                                            >>${DEST}
+  echo "Type=oneshot"                                                         >>${DEST}
+  echo "RemainAfterExit=true"                                                 >>${DEST}
+  echo "ExecStart=/usr/bin/diskdbpatch.sh -nrf"                               >>${DEST}
+  echo "ExecStop=/usr/bin/diskdbpatch.sh -nrf"                                >>${DEST}
+  echo                                                                        >>${DEST}
+  echo "[Install]"                                                            >>${DEST}
+  echo "WantedBy=multi-user.target"                                           >>${DEST}
+
+  mkdir -p /tmpRoot/lib/systemd/system/multi-user.target.wants
+  ln -sf /lib/systemd/system/diskdbpatch.service /tmpRoot/lib/systemd/system/multi-user.target.wants/diskdbpatch.service
+fi
