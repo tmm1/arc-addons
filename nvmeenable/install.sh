b57990fa1160155fe8ffa9108ffc53bafec21aa0
AuxXxilium <info@auxxxilium.tech>
2023-05-12 20:36:34 +0200

    addons: add nvmeenable
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- nvmeenable/install.sh
+++ nvmeenable/install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env ash
 
 if [ "${1}" = "late" ]; then
-  echo "Creating service to exec NVMe Storage"
-  cp -v /usr/bin/nvmestorage.sh /tmpRoot/usr/bin/nvmestorage.sh
-  DEST="/tmpRoot/lib/systemd/system/nvmestorage.service"
+  echo "Creating service to exec Enable NVMe"
+  cp -v /usr/bin/nvmeenable.sh /tmpRoot/usr/bin/nvmeenable.sh
+  DEST="/tmpRoot/lib/systemd/system/nvmeenable.service"
   echo "[Unit]"                                                                >${DEST}
-  echo "Description=Enable NVMe storage"                                      >>${DEST}
+  echo "Description=Enable NVMe as Storage"                                   >>${DEST}
   echo                                                                        >>${DEST}
   echo "[Service]"                                                            >>${DEST}
   echo "Type=oneshot"                                                         >>${DEST}
   echo "RemainAfterExit=true"                                                 >>${DEST}
-  echo "ExecStart=/usr/bin/nvmestorage.sh"                                    >>${DEST}
-  echo "ExecStop=/usr/bin/nvmestorage.sh"                                     >>${DEST}
+  echo "ExecStart=/usr/bin/nvmeenable.sh"                                     >>${DEST}
+  echo "ExecStop=/usr/bin/nvmeenable.sh"                                      >>${DEST}
   echo                                                                        >>${DEST}
   echo "[Install]"                                                            >>${DEST}
   echo "WantedBy=multi-user.target"                                           >>${DEST}
 
   mkdir -p /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -sf /lib/systemd/system/nvmestorage.service /tmpRoot/lib/systemd/system/multi-user.target.wants/nvmestorage.service
+  ln -sf /lib/systemd/system/nvmeenable.service /tmpRoot/lib/systemd/system/multi-user.target.wants/nvmeenable.service
 fi
