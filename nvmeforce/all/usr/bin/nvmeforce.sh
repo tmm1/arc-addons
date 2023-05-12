e0828183f81aeb18fdab5d5aa4ea5680a5080f9e
AuxXxilium <info@auxxxilium.tech>
2023-05-12 20:37:42 +0200

    addons: add nvmeforce and remove nvmestorage
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- nvmeforce/all/usr/bin/nvmeforce.sh
+++ nvmeforce/all/usr/bin/nvmeforce.sh
@@ -4,8 +4,8 @@
 # NVMe Storage Patch by AuxXxilium
 #
 
-scriptver="23.5.1"
-script=nvmestorage.sh
+scriptver="v23.5.1"
+script=nvmeforce.sh
 
 # Check BASH variable is is non-empty and posix mode is off, else abort with error.
 [ "$BASH" ] && ! shopt -qo posix || {
---
+++ nvmeforce/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ash
+
+if [ "${1}" = "late" ]; then
+  echo "Creating service to exec Force NVMe"
+  cp -v /usr/bin/nvmeforce.sh /tmpRoot/usr/bin/nvmeforce.sh
+  DEST="/tmpRoot/lib/systemd/system/nvmeforce.service"
+  echo "[Unit]"                                                                >${DEST}
+  echo "Description=Force formate NVMe as Storage"                            >>${DEST}
+  echo                                                                        >>${DEST}
+  echo "[Service]"                                                            >>${DEST}
+  echo "Type=oneshot"                                                         >>${DEST}
+  echo "RemainAfterExit=true"                                                 >>${DEST}
+  echo "ExecStart=/usr/bin/nvmeforce.sh"                                      >>${DEST}
+  echo "ExecStop=/usr/bin/nvmeforce.sh"                                       >>${DEST}
+  echo                                                                        >>${DEST}
+  echo "[Install]"                                                            >>${DEST}
+  echo "WantedBy=multi-user.target"                                           >>${DEST}
+
+  mkdir -p /tmpRoot/lib/systemd/system/multi-user.target.wants
+  ln -sf /lib/systemd/system/nvmeforce.service /tmpRoot/lib/systemd/system/multi-user.target.wants/nvmeforce.service
+fi
