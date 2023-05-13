9bd8d082fd2ed57e55b91ee4c503e8dc5bfa5088
AuxXxilium <info@auxxxilium.tech>
2023-05-13 18:03:43 +0200

    addons: surveillance: fix script and release
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- surveillance/all/usr/bin/surveillance.sh
+++ surveillance/all/usr/bin/surveillance.sh
@@ -9,18 +9,18 @@ if [ -f "${PATH1}/libssutils.so" ]; then
 
     cp ${PATH1}/libssutils.so ${PATH1}/libssutils.so.bak
     rm -f ${PATH1}/libssutils.so
-    wget https://github.com/AuxXxilium/Surveillance-Station/raw/main/lib/SurveillanceStation-x86_64/libssutils.so ${PATH1}/libssutils.so
+    wget -O ${PATH1}/libssutils.so https://github.com/AuxXxilium/Surveillance-Station-9/raw/main/lib/SurveillanceStation-x86_64/libssutils.so
     chown SurveillanceStation:SurveillanceStation ${PATH1}/libssutils.so
     chmod 0644 ${PATH1}/libssutils.so
 
     cp ${PATH2}/S82surveillance.sh ${PATH2}/S82surveillance.sh.bak
     rm -f ${PATH2}/S82surveillance.sh
-    wget https://raw.githubusercontent.com/AuxXxilium/Surveillance-Station/main/lib/license/S82surveillance.sh ${PATH2}/S82surveillance.sh
+    wget -O ${PATH2}/S82surveillance.sh https://raw.githubusercontent.com/AuxXxilium/Surveillance-Station-9/main/lib/license/S82surveillance.sh
     chown SurveillanceStation:SurveillanceStation ${PATH2}/S82surveillance.sh
     chmod 0755 ${PATH2}/S82surveillance.sh
 
     rm -f ${PATH2}/license.sh
-    wget https://raw.githubusercontent.com/AuxXxilium/Surveillance-Station/main/lib/license/license.sh ${PATH2}/license.sh
+    wget -O ${PATH2}/license.sh https://raw.githubusercontent.com/AuxXxilium/Surveillance-Station-9/main/lib/license/license.sh
     chown SurveillanceStation:SurveillanceStation ${PATH2}/license.sh
     chmod 0777 ${PATH2}/license.sh
 
