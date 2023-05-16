0b00dd69964db65fbdff1443c0336c4f601c1af5
AuxXxilium <info@auxxxilium.tech>
2023-05-16 22:01:08 +0200

    addons: nvmeenable: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- nvmeenable/install.sh
+++ nvmeenable/install.sh
@@ -3,6 +3,10 @@
 if [ "${1}" = "late" ]; then
   echo "Creating service to exec Enable NVMe"
   cp -v /usr/bin/nvmeenable.sh /tmpRoot/usr/bin/nvmeenable.sh
+  cp -v /usr/bin/bc /tmpRoot/usr/bin/bc
+  cp -v /usr/bin/od /tmpRoot/usr/bin/od
+  cp -v /usr/bin/tr /tmpRoot/usr/bin/tr
+  cp -v /usr/bin/xxd /tmpRoot/usr/bin/xxs
   DEST="/tmpRoot/lib/systemd/system/nvmeenable.service"
   echo "[Unit]"                                                                >${DEST}
   echo "Description=Enable NVMe as Storage"                                   >>${DEST}
