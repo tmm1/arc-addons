f1f112ef411a30c2378fc17440c9028707c45771
AuxXxilium <info@auxxxilium.tech>
2023-05-23 19:46:35 +0200

    addons: misc: fix typo
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- misc/install.sh
+++ misc/install.sh
@@ -52,7 +52,7 @@ if [ "${1}" = "late" ]; then
 
   # Intel GPU
   if [ -f /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf ]; then
-      INTELGPU=$(cat /proc/bus/pci/devices | grep -i i915 | wc -l)
+      INTELGPU=`cat /proc/bus/pci/devices | grep -i i915 | wc -l`
       if [ $INTELGPU -eq 0 ]; then
           echo "Intel GPU is not detected, disabling "
           ${SED_PATH} -i 's/^i915/# i915/g' /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf
