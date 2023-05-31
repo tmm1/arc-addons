f6eb4eb832479b6914b7f4ab518839103d48ac0b
AuxXxilium <info@auxxxilium.tech>
2023-05-31 20:53:30 +0200

    addons: rename surveillance to surveillancepatch
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- surveillancepatch/install.sh
+++ surveillancepatch/install.sh
@@ -2,24 +2,24 @@
 
 if [ "${1}" = "late" ]; then
   echo "Creating service to exec Surveillance Patch"
-  cp -vf /usr/sbin/surveillance.sh /tmpRoot/usr/sbin/surveillance.sh
+  cp -vf /usr/sbin/surveillancepatch.sh /tmpRoot/usr/sbin/surveillancepatch.sh
   cp -vf /usr/lib/libssutils.so /tmpRoot/usr/lib/libssutils.so
   cp -vf /usr/lib/license.sh /tmpRoot/usr/lib/license.sh
   cp -vf /usr/lib/S82surveillance.sh /tmpRoot/usr/lib/S82surveillance.sh
-  chmod 755 /tmpRoot/usr/sbin/surveillance.sh
+  chmod 755 /tmpRoot/usr/sbin/surveillancepatch.sh
 
-  DEST="/tmpRoot/lib/systemd/system/surveillance.service"
+  DEST="/tmpRoot/lib/systemd/system/surveillancepatch.service"
   echo "[Unit]"                                                                >${DEST}
   echo "Description=Surveillance Patch"                                       >>${DEST}
   echo                                                                        >>${DEST}
   echo "[Service]"                                                            >>${DEST}
   echo "Type=oneshot"                                                         >>${DEST}
   echo "RemainAfterExit=true"                                                 >>${DEST}
-  echo "ExecStart=/usr/sbin/surveillance.sh"                                  >>${DEST}
+  echo "ExecStart=/usr/sbin/surveillancepatch.sh"                             >>${DEST}
   echo                                                                        >>${DEST}
   echo "[Install]"                                                            >>${DEST}
   echo "WantedBy=multi-user.target"                                           >>${DEST}
 
   mkdir -p /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -sf /lib/systemd/system/surveillance.service /tmpRoot/lib/systemd/system/multi-user.target.wants/surveillance.service
+  ln -sf /lib/systemd/system/surveillancepatch.service /tmpRoot/lib/systemd/system/multi-user.target.wants/surveillancepatch.service
 fi
