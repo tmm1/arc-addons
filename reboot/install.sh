6330072b00714fdc1aaf6535380f0a837dc709bb
AuxXxilium <info@auxxxilium.tech>
2023-06-05 16:36:43 +0200

    addons: fix reboot script
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- reboot/install.sh
+++ reboot/install.sh
@@ -3,7 +3,8 @@
 if [ "${1}" = "late" ]; then
 if [ -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
 echo "insert RebootToArpl task"
-/tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db <<EOF
+export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib; /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db <<EOF
+DELETE FROM task WHERE task_name LIKE 'RebootToArpl';
 INSERT INTO task VALUES('RebootToArpl', '', 'shutdown', '', 0, 0, 0, 0, '', 0, '/usr/bin/arpl-reboot.sh "config"', 'script', '{}', '', '', '{}', '{}');
 EOF
 else
