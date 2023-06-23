04aa196c634d68ae0d4b84f0cfcfb1a85cf54a06
AuxXxilium <info@auxxxilium.tech>
2023-06-23 17:34:38 +0200

    addons: reboot: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- reboot/install.sh
+++ reboot/install.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env ash
 
 if [ "${1}" = "late" ]; then
-if [ -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
-echo "insert RebootToArpl task"
+if [ ! -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
+  echo "copy esynoscheduler.db"
+  mkdir -p /tmpRoot/usr/syno/etc/esynoscheduler
+  cp -f /addons/esynoscheduler.db /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db
+fi
+echo "insert RebootToArpl task to esynoscheduler.db"
 export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib; /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db <<EOF
 DELETE FROM task WHERE task_name LIKE 'RebootToArpl';
 INSERT INTO task VALUES('RebootToArpl', '', 'shutdown', '', 0, 0, 0, 0, '', 0, '/usr/bin/arpl-reboot.sh "config"', 'script', '{}', '', '', '{}', '{}');
 EOF
-else
-echo "copy RebootToArpl task db"
-mkdir -p /tmpRoot/usr/syno/etc/esynoscheduler
-cp -f /usr/lib/esynoscheduler.db /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db
-fi
 fi
