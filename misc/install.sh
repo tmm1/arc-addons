34fe7928c18288dafc6b19d17d83304b19a3cccb
AuxXxilium <info@auxxxilium.tech>
2023-06-29 16:32:40 +0200

    addons: misc: update to latest
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- misc/install.sh
+++ misc/install.sh
@@ -55,7 +55,7 @@ if [ "${1}" = "late" ]; then
   # Intel GPU
   if [ -f /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf ] && [ -f /tmpRoot/usr/lib/modules/i915.ko ]; then
     export LD_LIBRARY_PATH=/tmpRoot/usr/bin:/tmpRoot/usr/lib:${LD_LIBRARY_PATH}
-    GPU="$(${LSPCI_PATH} -n | grep 0300 | cut -d " " -f 3 | ${SED_PATH} -e 's/://g')"
+    GPU="$(${LSPCI_PATH} -n | grep 0300 | grep 8086 | cut -d " " -f 3 | ${SED_PATH} -e 's/://g')"
     echo "${GPU}" >/tmpRoot/root/i915.GPU
     if [ -n "${GPU}" -a $(echo -n "${GPU}" | wc -c) -eq 8 ]; then
       if [ $(grep -i ${GPU} /usr/bin/i915ids | wc -l) -eq 0 ]; then
