73835b85260221d2c8ce81a6dda77d47a25e6c28
AuxXxilium <info@auxxxilium.tech>
2023-07-03 13:51:12 +0200

    addons: amepatch: new dynamic patch
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- amepatch/all/usr/sbin/amepatch.sh
+++ amepatch/all/usr/sbin/amepatch.sh
@@ -1,42 +1,60 @@
 #!/usr/bin/env ash
 
-scriptver="23.6.1"
+scriptver="23.7.1"
 script=AmePatch
 repo="AuxXxilium/arc-addons"
 
 if [ -d "/var/packages/CodecPack" ]; then
-
-    PATH1="/volume1/@appstore/CodecPack/usr/lib"
-    SPATCH="/usr/lib"
-
     /usr/syno/bin/synopkg stop CodecPack
-    sleep 15
+    sleep 10
 
-    cp -f ${PATH1}/libsynoame-license.so ${PATH1}/libsynoame-license.so.bak
-    rm -f ${PATH1}/libsynoame-license.so
-    cp -f ${SPATCH}/libsynoame-license.so ${PATH1}/libsynoame-license.so
-    chown CodecPack:CodecPack ${PATH1}/libsynoame-license.so
-    chmod 0644 ${PATH1}/libsynoame-license.so
+    majorversion=$(cat /etc.defaults/VERSION | grep majorversion | sed 's/majorversion=//' | tr -d '"')
+    minorversion=$(cat /etc.defaults/VERSION | grep minorversion | sed 's/minorversion=//' | tr -d '"')
+    values=('669066909066906690' 'B801000000' '30')
+    hex_values=('1F28' '48F5' '4921' '4953' '4975' '9AC8')
+    indices=(0 1 1 1 1 2)
+    cp_usr_path='/var/packages/CodecPack/target/usr'
+    so="$cp_usr_path/lib/libsynoame-license.so"
+    lic="/usr/syno/etc/license/data/ame/offline_license.json"
+    licsig="/usr/syno/etc/license/data/ame/offline_license.sig"
 
-    ROOTDISK_PATH=""
-    json=${ROOTDISK_PATH}/usr/syno/etc/license/data/ame/offline_license.json
-    # apparmor=${ROOTDISK_PATH}/var/packages/CodecPack/target/apparmor
-    apparmor=${ROOTDISK_PATH}/volume1/@appstore/CodecPack/apparmor
-
-    mkdir -p "${ROOTDISK_PATH}/usr/syno/etc/license/data/ame"
-    echo '[{"appType": 14, "appName": "ame", "follow": ["device"], "server_time": 1666000000, "registered_at": 1651000000, "expireTime": 0, "status": "valid", "firstActTime": 1651000001, "extension_gid": null, "licenseCode": "0", "duration": 1576800000, "attribute": {"codec": "hevc", "type": "free"}, "licenseContent": 1}, {"appType": 14, "appName": "ame", "follow": ["device"], "server_time": 1666000000, "registered_at": 1651000000, "expireTime": 0, "status": "valid", "firstActTime": 1651000001, "extension_gid": null, "licenseCode": "0", "duration": 1576800000, "attribute": {"codec": "aac", "type": "free"}, "licenseContent": 1}]' > "${json}"
-
-    /usr/syno/etc/rc.sysv/apparmor.sh remove_packages_profile 0 CodecPack
-    # disable apparmor check for AME
-    if [ -e "${apparmor}" ]; then
-        mv -f "${apparmor}" "${apparmor}.bak"
+    if [[ "$majorversion" -eq "7" && "$minorversion" -le "1" ]]; then
+        expected_checksum='fcc1084f4eadcf5855e6e8494fb79e23'
+        hex_values=('1F28' '48F5' '4921' '4953' '4975' '9AC8')
+        content= '[{"appType": 14, "appName": "ame", "follow": ["device"], "server_time": 1666000000, "registered_at": 1651000000, "expireTime": 0, "status": "valid", "firstActTime": 1651000001, "extension_gid": null, "licenseCode": "0", "duration": 1576800000, "attribute": {"codec": "hevc", "type": "free"}, "licenseContent": 1}, {"appType": 14, "appName": "ame", "follow": ["device"], "server_time": 1666000000, "registered_at": 1651000000, "expireTime": 0, "status": "valid", "firstActTime": 1651000001, "extension_gid": null, "licenseCode": "0", "duration": 1576800000, "attribute": {"codec": "aac", "type": "free"}, "licenseContent": 1}]'
+    elif [[ "$majorversion" -eq "7" && "$minorversion" -eq "2" ]]; then
+        expected_checksum='09e3adeafe85b353c9427d93ef0185e9'
+        hex_values=('3718' '60A5' '60D1' '6111' '6137' 'B5F0')
+        content='[{"attribute": {"codec": "hevc", "type": "free"}, "status": "valid", "extension_gid": null, "expireTime": 0, "appName": "ame", "follow": ["device"], "duration": 1576800000, "appType": 14, "licenseContent": 1, "registered_at": 1649315995, "server_time": 1685421618, "firstActTime": 1649315995, "licenseCode": "0"}, {"attribute": {"codec": "aac", "type": "free"}, "status": "valid", "extension_gid": null, "expireTime": 0, "appName": "ame", "follow": ["device"], "duration": 1576800000, "appType": 14, "licenseContent": 1, "registered_at": 1649315995, "server_time": 1685421618, "firstActTime": 1649315995, "licenseCode": "0"}]'
     fi
 
-    /var/packages/CodecPack/target/usr/bin/synoame-bin-auto-install-needed-codec
+    if [ "$(md5sum -b "$so" | awk '{print $1}')" != "$expected_checksum" ]; then
+        echo -e "AME Patch: MD5 mismatch"
+        exit 1
+    fi
 
-    echo -e "AME Patch: Successfull!"
+    for ((i = 0; i < ${#hex_values[@]}; i++)); do
+        offset=$(( 0x${hex_values[i]} + 0x8000 ))
+        value=${values[indices[i]]}
+        printf '%s' "$value" | xxd -r -p | dd of="$so" bs=1 seek="$offset" conv=notrunc
+        if [[ $? -ne 0 ]]; then
+            echo -e "AME Patch: Error while writing to file!"
+            exit 1
+        fi
+    done
+
+    mkdir -p "$(dirname "$lic")"
+    echo "$content" > "$lic"
+
+	if "$cp_usr_path/bin/synoame-bin-check-license"; then
+        "$cp_usr_path/bin/synoame-bin-auto-install-needed-codec"
+        /var/packages/CodecPack/target/usr/bin/synoame-bin-auto-install-needed-codec
+        echo -e "AME Patch: Successful!"
+    else
+        echo -e "AME Patch: Unsuccessful!"
+        exit 1
+   	fi
 
     sleep 5
     /usr/syno/bin/synopkg start CodecPack
-
 fi
