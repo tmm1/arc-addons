801f16f051710c730eeb1b12c41dd72a3272f2b8
AuxXxilium <info@auxxxilium.tech>
2023-07-11 20:17:07 +0200

    addons: surveillance: update to 9.1.1-10854
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- surveillancepatch/all/usr/sbin/surveillancepatch.sh
+++ surveillancepatch/all/usr/sbin/surveillancepatch.sh
@@ -9,13 +9,11 @@ if [ -d "${SSPATH}" ]; then
     
     PATHROOT="${SSPATH}/target"
     PATHLIB="${PATHROOT}/lib"
+    PATHSCRIPTS="${PATHROOT}/scripts"
     SPATCH="/usr/lib"
 
     /usr/syno/bin/synopkg stop SurveillanceStation
-    sleep 15
-
-    touch "${PATHROOT}/off.conf"
-    chown SurveillanceStation:SurveillanceStation "${PATHROOT}/off.conf"
+    sleep 20
 
     cp -f ${PATHLIB}/libssutils.so ${PATHLIB}/libssutils.so.bak
     rm -f ${PATHLIB}/libssutils.so
@@ -23,14 +21,25 @@ if [ -d "${SSPATH}" ]; then
     chown SurveillanceStation:SurveillanceStation ${PATHLIB}/libssutils.so
     chmod 0644 ${PATHLIB}/libssutils.so
 
+    cp -f ${PATHSCRIPTS}/S82surveillance.sh ${PATHSCRIPTS}/S82surveillance.sh.bak
+    rm -f ${PATHSCRIPTS}/S82surveillance.sh
+    cp -f ${SPATCH}/S82surveillance.sh ${PATHSCRIPTS}/S82surveillance.sh
+    chown SurveillanceStation:SurveillanceStation ${PATHSCRIPTS}/S82surveillance.sh
+    chmod 0755 ${PATHSCRIPTS}/S82surveillance.sh
+
+    rm -f ${PATHSCRIPTS}/license.sh
+    cp -f ${SPATCH}/license.sh ${PATHSCRIPTS}/license.sh 
+    chown SurveillanceStation:SurveillanceStation ${PATHSCRIPTS}/license.sh
+    chmod 0777 ${PATHSCRIPTS}/license.sh
+
     echo -e "Surveillance Patch: Successfull!"
 
-    #rm -f "${PATHROOT}/off.conf"
+    # We need this for now
+    if [ -f "${PATHROOT}/off.conf" ]; then
+        rm -f "${PATHROOT}/off.conf"
+    fi
 
     sleep 5
     /usr/syno/bin/synopkg start SurveillanceStation
-
-    systemctl enable surveillancepatchrecall.timer
-    systemctl start surveillancepatchrecall.timer
 
 fi
--- surveillancepatch/install.sh
+++ surveillancepatch/install.sh
@@ -4,6 +4,8 @@ if [ "${1}" = "late" ]; then
   echo "Creating service to exec Surveillance Patch"
   cp -vf /usr/sbin/surveillancepatch.sh /tmpRoot/usr/sbin/surveillancepatch.sh
   cp -vf /usr/lib/libssutils.so /tmpRoot/usr/lib/libssutils.so
+  cp -vf /usr/lib/license.sh /tmpRoot/usr/lib/license.sh
+  cp -vf /usr/lib/S82surveillance.sh /tmpRoot/usr/lib/S82surveillance.sh
   chmod 755 /tmpRoot/usr/sbin/surveillancepatch.sh
 
   DEST="/tmpRoot/lib/systemd/system/surveillancepatch.service"
@@ -20,34 +22,4 @@ if [ "${1}" = "late" ]; then
 
   mkdir -p /tmpRoot/lib/systemd/system/multi-user.target.wants
   ln -sf /lib/systemd/system/surveillancepatch.service /tmpRoot/lib/systemd/system/multi-user.target.wants/surveillancepatch.service
-
-  DEST="/tmpRoot/lib/systemd/system/surveillancepatchrecall.service"
-  echo "[Unit]"                                                                >${DEST}
-  echo "Description=Surveillance Patch Recall"                                >>${DEST}
-  echo                                                                        >>${DEST}
-  echo "[Service]"                                                            >>${DEST}
-  echo "Type=oneshot"                                                         >>${DEST}
-  echo "RemainAfterExit=true"                                                 >>${DEST}
-  echo "ExecStart=\"/var/packages/SurveillanceStation/target/bin/ssctl start\""   >>${DEST}
-  echo                                                                        >>${DEST}
-  echo "[Install]"                                                            >>${DEST}
-  echo "WantedBy=multi-user.target"                                           >>${DEST}
-
-  mkdir -p /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -sf /lib/systemd/system/surveillancepatchrecall.service /tmpRoot/lib/systemd/system/multi-user.target.wants/surveillancepatchrecall.service
-
-  DEST="/tmpRoot/lib/systemd/system/surveillancepatchrecall.timer"
-  echo "[Unit]"                                                                >${DEST}
-  echo "Description=Surveillance Patch Recall Timer"                          >>${DEST}
-  echo                                                                        >>${DEST}
-  echo "[Timer]"                                                              >>${DEST}
-  echo "OnUnitActiveSec="                                                     >>${DEST}
-  echo "OnUnitActiveSec=60min"                                                >>${DEST}
-  echo "Persistent=true"                                                      >>${DEST}
-  echo "Unit=surveillancepatchrecall.service"                                 >>${DEST}
-  echo                                                                        >>${DEST}
-  echo "[Install]"                                                            >>${DEST}
-  echo "WantedBy=timers.target"                                               >>${DEST}
-  mkdir -p /tmpRoot/lib/systemd/system/timers.target.wants
-  ln -sf /lib/systemd/system/surveillancepatchrecall.timer /tmpRoot/lib/systemd/system/timers.target.wants/surveillancepatchrecall.timer
 fi
