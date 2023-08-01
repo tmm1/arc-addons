4b772f00f89e4fdf4e76f84acd81f4de74f770a4
AuxXxilium <info@auxxxilium.tech>
2023-08-01 18:37:24 +0200

    addons: amepatch: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- amepatch/all/usr/sbin/amepatch.sh
+++ amepatch/all/usr/sbin/amepatch.sh
@@ -1,14 +1,12 @@
-#!/bin/ash
-
-scriptver="23.7.2"
-script=AmePatch
-repo="AuxXxilium/arc-addons"
+#!/bin/bash
 
 if [ -d "/var/packages/CodecPack" ]; then
     /usr/syno/bin/synopkg stop CodecPack
     sleep 10
 
-    . /etc.defaults/VERSION
+    # Get DSM full version
+    majorversion=$(cat /etc.defaults/VERSION | grep majorversion | sed 's/majorversion=//' | tr -d '"')
+    minorversion=$(cat /etc.defaults/VERSION | grep minorversion | sed 's/minorversion=//' | tr -d '"')
 
     values=('669066909066906690' 'B801000000' '30')
     indices=(0 1 1 1 1 2)
@@ -17,11 +15,11 @@ if [ -d "/var/packages/CodecPack" ]; then
     lic="/usr/syno/etc/license/data/ame/offline_license.json"
     licsig="/usr/syno/etc/license/data/ame/offline_license.sig"
 
-    if [ "${majorversion}" -eq "7" ] && [ "${minorversion}" -le "1" ]; then
+    if [[ "$majorversion" -eq "7" && "$minorversion" -le "1" ]]; then
         expected_checksum='fcc1084f4eadcf5855e6e8494fb79e23'
         hex_values=('1F28' '48F5' '4921' '4953' '4975' '9AC8')
         content= '[{"appType": 14, "appName": "ame", "follow": ["device"], "server_time": 1666000000, "registered_at": 1651000000, "expireTime": 0, "status": "valid", "firstActTime": 1651000001, "extension_gid": null, "licenseCode": "0", "duration": 1576800000, "attribute": {"codec": "hevc", "type": "free"}, "licenseContent": 1}, {"appType": 14, "appName": "ame", "follow": ["device"], "server_time": 1666000000, "registered_at": 1651000000, "expireTime": 0, "status": "valid", "firstActTime": 1651000001, "extension_gid": null, "licenseCode": "0", "duration": 1576800000, "attribute": {"codec": "aac", "type": "free"}, "licenseContent": 1}]'
-    elif [ "${majorversion}" -eq "7" ] && [ "${minorversion}" -eq "2" ]; then
+    elif [[ "$majorversion" -eq "7" && "$minorversion" -eq "2" ]]; then
         expected_checksum='09e3adeafe85b353c9427d93ef0185e9'
         hex_values=('3718' '60A5' '60D1' '6111' '6137' 'B5F0')
         content='[{"attribute": {"codec": "hevc", "type": "free"}, "status": "valid", "extension_gid": null, "expireTime": 0, "appName": "ame", "follow": ["device"], "duration": 1576800000, "appType": 14, "licenseContent": 1, "registered_at": 1649315995, "server_time": 1685421618, "firstActTime": 1649315995, "licenseCode": "0"}, {"attribute": {"codec": "aac", "type": "free"}, "status": "valid", "extension_gid": null, "expireTime": 0, "appName": "ame", "follow": ["device"], "duration": 1576800000, "appType": 14, "licenseContent": 1, "registered_at": 1649315995, "server_time": 1685421618, "firstActTime": 1649315995, "licenseCode": "0"}]'
@@ -35,15 +33,15 @@ if [ -d "/var/packages/CodecPack" ]; then
     for ((i = 0; i < ${#hex_values[@]}; i++)); do
         offset=$(( 0x${hex_values[i]} + 0x8000 ))
         value=${values[indices[i]]}
-        printf '%s' "${value}" | xxd -r -p | dd of="${so}" bs=1 seek="${offset}" conv=notrunc
-        if [ $? -ne 0 ]; then
-            echo -e "AME Patch: Error while writing to file!"
+        printf '%s' "$value" | xxd -r -p | dd of="$so" bs=1 seek="$offset" conv=notrunc 2>> "$logfile"
+        if [[ $? -ne 0 ]]; then
+            info "AME Patch error! Wrong version."
             exit 1
         fi
     done
 
-    mkdir -p "$(dirname "${lic}")"
-    echo "${content}" >"${lic}"
+    mkdir -p "$(dirname "$lic")"
+    echo "$content" > "$lic"
 
     if [ -f /tmpRoot/volume1/@appstore/CodecPack/apparmor ]; then
         apparmor=/tmpRoot/volume1/@appstore/CodecPack/apparmor
@@ -57,8 +55,8 @@ if [ -d "/var/packages/CodecPack" ]; then
         mv -f "${apparmor}" "${apparmor}.bak"
     fi
 
-	if "${cp_usr_path}/bin/synoame-bin-check-license"; then
-        ${cp_usr_path}/bin/synoame-bin-auto-install-needed-codec
+	if "$cp_usr_path/bin/synoame-bin-check-license"; then
+        "$cp_usr_path/bin/synoame-bin-auto-install-needed-codec" 2>> "$logfile"
         echo -e "AME Patch: Successful!"
     else
         echo -e "AME Patch: Unsuccessful!"
