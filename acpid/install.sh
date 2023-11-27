8b204c7b0616ae9021addc6a5a5f16b0c7b4fcb4
AuxXxilium <info@auxxxilium.tech>
2023-11-27 22:43:43 +0100

    Revert "acpid: update"
    This reverts commit 59e8279b156e70da3102e2163704078b9d9f899a.

--- acpid/install.sh
+++ acpid/install.sh
@@ -7,21 +7,20 @@ if [ "${1}" = "late" ]; then
   cp -vf /usr/sbin/acpid /tmpRoot/usr/sbin/acpid
   cp -vf /etc/acpi/power.sh /tmpRoot/etc/acpi/power.sh
   DEST=/tmpRoot/lib/systemd/system/acpid.service
-  echo "[Unit]"                                  >${DEST}
-  echo "Description=ACPI Daemon"                >>${DEST}
-  echo "DefaultDependencies=no"                 >>${DEST}
-  echo "IgnoreOnIsolate=true"                   >>${DEST}
-  echo "After=multi-user.target"                >>${DEST}
-  echo                                          >>${DEST}
-  echo "[Service]"                              >>${DEST}
-  echo "Restart=always"                         >>${DEST}
-  echo "RestartSec=30"                          >>${DEST}
-  echo "ExecStartPre=-/sbin/modprobe button"    >>${DEST}
-  echo "ExecStart=/usr/sbin/acpid -f"           >>${DEST}
-  echo "ExecStopPost=-/sbin/modprobe -r button" >>${DEST}
-  echo                                          >>${DEST}
-  echo "[X-Synology]"                           >>${DEST}
-  echo "Author=Virtualization Team"             >>${DEST}
+  echo "[Unit]"                               >${DEST}
+  echo "Description=ACPI Daemon"             >>${DEST}
+  echo "DefaultDependencies=no"              >>${DEST}
+  echo "IgnoreOnIsolate=true"                >>${DEST}
+  echo "After=multi-user.target"             >>${DEST}
+  echo                                       >>${DEST}
+  echo "[Service]"                           >>${DEST}
+  echo "Restart=always"                      >>${DEST}
+  echo "RestartSec=30"                       >>${DEST}
+  echo "PIDFile=/var/run/acpid.pid"          >>${DEST}
+  echo "ExecStart=/usr/sbin/acpid -f"        >>${DEST}
+  echo                                       >>${DEST}
+  echo "[X-Synology]"                        >>${DEST}
+  echo "Author=Virtualization Team"          >>${DEST}
 
   mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
   ln -vsf /lib/systemd/system/acpid.service /tmpRoot/lib/systemd/system/multi-user.target.wants/acpid.service
