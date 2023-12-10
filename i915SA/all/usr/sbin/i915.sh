1a5d01cbd504f6b4244f3f4a49eededb9e179768
AuxXxilium <info@auxxxilium.tech>
2023-12-10 15:59:58 +0100

    i915SA: update load script
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- i915SA/all/usr/sbin/i915.sh
+++ i915SA/all/usr/sbin/i915.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-insmod i2c-algo-bit.ko
-insmod backport-sa6400-export.ko
-insmod backport-dma-buf.ko
-insmod hdmi.ko
-insmod backlight.ko
-insmod drm_panel_orientation_quirks.ko
-insmod drm.ko
-insmod fbdev.ko
-insmod fbcore.ko
-insmod drm_kms_helper.ko
-insmod backport-sa6400.ko
-insmod drm_mipi_dsi.ko
-insmod video.ko
-insmod i915.ko enable_guc=2
+insmod /usr/lib/modules/i2c-algo-bit.ko
+insmod /usr/lib/modules/backport-sa6400-export.ko
+insmod /usr/lib/modules/backport-dma-buf.ko
+insmod /usr/lib/modules/hdmi.ko
+insmod /usr/lib/modules/backlight.ko
+insmod /usr/lib/modules/drm_panel_orientation_quirks.ko
+insmod /usr/lib/modules/drm.ko
+insmod /usr/lib/modules/fbdev.ko
+insmod /usr/lib/modules/fbcore.ko
+insmod /usr/lib/modules/drm_kms_helper.ko
+insmod /usr/lib/modules/backport-sa6400.ko
+insmod /usr/lib/modules/drm_mipi_dsi.ko
+insmod /usr/lib/modules/video.ko
+insmod /usr/lib/modules/i915.ko enable_guc=2
