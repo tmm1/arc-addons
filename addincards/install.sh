4047d2fa92fb098daac0748ecbffa5d74188aaa4
AuxXxilium <info@auxxxilium.tech>
2024-01-04 19:48:14 +0100

    addincards: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- addincards/install.sh
+++ addincards/install.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env ash
 
 if [ "${1}" = "late" ]; then
-  echo "Installing daemon for Addincards"
-  cp -vf /usr/sbin/addincards.sh /tmpRoot/usr/sbin/addincards.sh
+  echo "Installing daemon for addincards"
 
-  DEST="/tmpRoot/lib/systemd/system/addincards.service"
-  echo "[Unit]"                                        >${DEST}
-  echo "Description=update Add-in cards and usb.map"  >>${DEST}
-  echo "After=multi-user.target"                      >>${DEST}
-  echo                                                >>${DEST}
-  echo "[Service]"                                    >>${DEST}
-  echo "Type=oneshot"                                 >>${DEST}
-  echo "RemainAfterExit=true"                         >>${DEST}
-  echo "ExecStart=/usr/sbin/addincards.sh"            >>${DEST}
-  echo                                                >>${DEST}
-  echo "[Install]"                                    >>${DEST}
-  echo "WantedBy=multi-user.target"                   >>${DEST}
+  MODEL="$(cat /proc/sys/kernel/syno_hw_version)"
+  FILE="/tmpRoot/usr/syno/etc.defaults/adapter_cards.conf"
 
-  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -vsf /lib/systemd/system/addincards.service /tmpRoot/lib/systemd/system/multi-user.target.wants/addincards.service
+  [ ! -f "${FILE}.bak" ] && cp -f "${FILE}" "${FILE}.bak"
+  echo -n "" >"${FILE}"
+  for N in $(cat "${FILE}.bak" | grep '\['); do
+    echo "${N}" >>"${FILE}"
+    echo "${MODEL}=yes" >>"${FILE}"
+  done
 fi
