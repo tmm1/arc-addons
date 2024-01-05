01b702b5245350a464a874125039008f7205b482
AuxXxilium <info@auxxxilium.tech>
2024-01-05 18:19:19 +0100

    i915SA: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- i915SA/all/usr/sbin/i915.sh
+++ i915SA/all/usr/sbin/i915.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
-insmod /usr/lib/modules/i2c-algo-bit.ko
-insmod /usr/lib/modules/backport-sa6400-export.ko
-insmod /usr/lib/modules/backport-dma-buf.ko
-insmod /usr/lib/modules/hdmi.ko
-insmod /usr/lib/modules/backlight.ko
-insmod /usr/lib/modules/drm_panel_orientation_quirks.ko
-insmod /usr/lib/modules/drm.ko
-insmod /usr/lib/modules/fbdev.ko
-insmod /usr/lib/modules/fbcore.ko
-insmod /usr/lib/modules/drm_kms_helper.ko
-insmod /usr/lib/modules/backport-sa6400.ko
-insmod /usr/lib/modules/drm_mipi_dsi.ko
-insmod /usr/lib/modules/video.ko
-insmod /usr/lib/modules/i915.ko enable_guc=2
+if [ -f "/usr/i915/lib/modules/i915.ko"]; then
+    insmod /usr/i915/lib/modules/i2c-algo-bit.ko
+    insmod /usr/i915/lib/modules/backport-sa6400-export.ko
+    insmod /usr/i915/lib/modules/backport-dma-buf.ko
+    insmod /usr/i915/lib/modules/hdmi.ko
+    insmod /usr/i915/lib/modules/backlight.ko
+    insmod /usr/i915/lib/modules/drm_panel_orientation_quirks.ko
+    insmod /usr/i915/lib/modules/drm.ko
+    insmod /usr/i915/lib/modules/fbdev.ko
+    insmod /usr/i915/lib/modules/fbcore.ko
+    insmod /usr/i915/lib/modules/drm_kms_helper.ko
+    insmod /usr/i915/lib/modules/backport-sa6400.ko
+    insmod /usr/i915/lib/modules/drm_mipi_dsi.ko
+    insmod /usr/i915/lib/modules/video.ko
+    insmod /usr/i915/lib/modules/i915.ko enable_guc=2
+elif [ -f "/tmpRoot/usr/i915/lib/modules/i915.ko"]; then
+    insmod /tmpRoot/usr/i915/lib/modules/i2c-algo-bit.ko
+    insmod /tmpRoot/usr/i915/lib/modules/backport-sa6400-export.ko
+    insmod /tmpRoot/usr/i915/lib/modules/backport-dma-buf.ko
+    insmod /tmpRoot/usr/i915/lib/modules/hdmi.ko
+    insmod /tmpRoot/usr/i915/lib/modules/backlight.ko
+    insmod /tmpRoot/usr/i915/lib/modules/drm_panel_orientation_quirks.ko
+    insmod /tmpRoot/usr/i915/lib/modules/drm.ko
+    insmod /tmpRoot/usr/i915/lib/modules/fbdev.ko
+    insmod /tmpRoot/usr/i915/lib/modules/fbcore.ko
+    insmod /tmpRoot/usr/i915/lib/modules/drm_kms_helper.ko
+    insmod /tmpRoot/usr/i915/lib/modules/backport-sa6400.ko
+    insmod /tmpRoot/usr/i915/lib/modules/drm_mipi_dsi.ko
+    insmod /tmpRoot/usr/i915/lib/modules/video.ko
+    insmod /tmpRoot/usr/i915/lib/modules/i915.ko enable_guc=2
+fi
