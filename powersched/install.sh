31f21abd2aafaa0674eb33e9479d598701c2aeb5
AuxXxilium <info@auxxxilium.tech>
2024-01-19 15:58:31 +0100

    powersched: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- powersched/install.sh
+++ powersched/install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env ash
 
+SED_PATH='/tmpRoot/usr/bin/sed'
+
 if [ "${1}" = "late" ]; then
-  echo "Powersched: Installing powersched tools"
-  SED_PATH='/tmpRoot/usr/bin/sed'
+  echo "Installing powersched tools"
   cp -vf /usr/sbin/powersched /tmpRoot/usr/sbin/powersched
   chmod 755 /tmpRoot/usr/sbin/powersched
   # Clean old entries
-  ${SED_PATH} -i '/\/usr\/sbin\/powersched/d' /tmpRoot/etc/crontab 
+  ${SED_PATH} -i '/\/usr\/sbin\/powersched/d' /tmpRoot/etc/crontab
   # Add line to crontab, execute each minute
   echo "*       *       *       *       *       root    /usr/sbin/powersched #arpl powersched addon" >>/tmpRoot/etc/crontab
 fi
