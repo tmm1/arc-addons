5395f60da581599c36cfb18c20b88d83a7c6fdb9
AuxXxilium <info@auxxxilium.tech>
2024-01-20 13:33:26 +0100

    hostspatch: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- hostspatch/install.sh
+++ hostspatch/install.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/env ash
 
 if [ "${1}" = "late" ]; then
-  # Define the entries to be added
-  ENTRIES=("127.0.0.1 checkip.synology.com" "::1 checkipv6.synology.com")
-
-  # Loop over each entry
-  for ENTRY in "${ENTRIES[@]}"
-  do
-    if [ -f /etc/hosts ]; then
-        # Check if the entry is already in the file
-        if grep -Fxq "$ENTRY" /etc/hosts; then
-            echo "Entry $ENTRY already exists"
-        else
-            echo "Entry $ENTRY does not exist, adding now"
-            echo "$ENTRY" >> /etc/hosts
-        fi
-    fi
-    if [ -f /tmpRoot/etc/hosts ]; then
-        if grep -Fxq "$ENTRY" /tmpRoot/etc/hosts; then
-            echo "Entry $ENTRY already exists"
-        else
-            echo "Entry $ENTRY does not exist, adding now"
-            echo "$ENTRY" >> /tmpRoot/etc/hosts
-        fi
-    fi
-  done
-fi
-
-if [ "${1}" = "late" ]; then
   echo "Creating service to exec Hostspatch"
   cp -vf /usr/sbin/hostspatch.sh /tmpRoot/usr/sbin/hostspatch.sh
   DEST="/tmpRoot/lib/systemd/system/hostspatch.service"
