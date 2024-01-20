11ced8dc96f19dbb8221d8a7bca8ac572744f397
AuxXxilium <info@auxxxilium.tech>
2024-01-20 13:20:50 +0100

    hostspatch: set as service
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- hostspatch/install.sh
+++ hostspatch/install.sh
@@ -26,3 +26,22 @@ if [ "${1}" = "late" ]; then
     fi
   done
 fi
+
+if [ "${1}" = "late" ]; then
+  echo "Creating service to exec Hostspatch"
+  cp -vf /usr/sbin/hostspatch.sh /tmpRoot/usr/sbin/hostspatch.sh
+  DEST="/tmpRoot/lib/systemd/system/hostspatch.service"
+  echo "[Unit]"                                                                >${DEST}
+  echo "Description=Enable CPU Info"                                          >>${DEST}
+  echo                                                                        >>${DEST}
+  echo "[Service]"                                                            >>${DEST}
+  echo "Type=oneshot"                                                         >>${DEST}
+  echo "RemainAfterExit=true"                                                 >>${DEST}
+  echo "ExecStart=/usr/sbin/hostspatch.sh"                                    >>${DEST}
+  echo                                                                        >>${DEST}
+  echo "[Install]"                                                            >>${DEST}
+  echo "WantedBy=multi-user.target"                                           >>${DEST}
+
+  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
+  ln -vsf /lib/systemd/system/hostspatch.service /tmpRoot/lib/systemd/system/multi-user.target.wants/hostspatch.service
+fi
