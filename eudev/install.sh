ab690cbed9709ee81d1c751312422d52e0af43ea
AuxXxilium <info@auxxxilium.tech>
2024-01-29 20:17:19 +0100

    eudev: reenable full firmware/module copy (possible issue with SA6400)
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- eudev/install.sh
+++ eudev/install.sh
@@ -36,11 +36,9 @@ elif [ "${1}" = "late" ]; then
 
   echo "eudev: copy Modules and Firmware"
   export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
-<<DISABLED
   /tmpRoot/bin/cp -vrf /usr/lib/firmware/* /tmpRoot/usr/lib/firmware/
   /tmpRoot/bin/cp -vrf /usr/lib/modules/* /tmpRoot/usr/lib/modules/
   /usr/sbin/depmod -a -b /tmpRoot/
-DISABLED
 
   echo "eudev: copy Rules"
   cp -vf /usr/lib/udev/rules.d/* /tmpRoot/usr/lib/udev/rules.d/
