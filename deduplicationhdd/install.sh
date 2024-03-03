c217dbce1a1c38edf7be2585bf67abdba6b26d20
AuxXxilium <info@auxxxilium.tech>
2024-03-03 18:37:22 +0100

    deduplication: add back for hdd (use at own risk)
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- deduplicationhdd/install.sh
+++ deduplicationhdd/install.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env ash
 
 if [ "${1}" = "late" ]; then
-  echo "Creating service to exec deduplication"
-  cp -vf /usr/sbin/deduplication.sh /tmpRoot/usr/sbin/deduplication.sh
+  echo "Creating service to exec deduplicationhdd"
+  cp -vf /usr/sbin/deduplicationhdd.sh /tmpRoot/usr/sbin/deduplicationhdd.sh
   
-  DEST="/tmpRoot/usr/lib/systemd/system/deduplication.service"
+  DEST="/tmpRoot/usr/lib/systemd/system/deduplicationhdd.service"
   echo "[Unit]"                                                                >${DEST}
-  echo "Description=Enable Deduplication"                                     >>${DEST}
+  echo "Description=Enable Deduplication HDD"                                 >>${DEST}
   echo                                                                        >>${DEST}
   echo "[Service]"                                                            >>${DEST}
   echo "Type=oneshot"                                                         >>${DEST}
   echo "RemainAfterExit=true"                                                 >>${DEST}
-  echo "ExecStart=/usr/bin/deduplication.sh --tiny -e"                        >>${DEST}
+  echo "ExecStart=/usr/bin/deduplicationhdd.sh --hdd -e"                      >>${DEST}
   echo                                                                        >>${DEST}
   echo "[Install]"                                                            >>${DEST}
   echo "WantedBy=multi-user.target"                                           >>${DEST}
 
   mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -vsf /usr/lib/systemd/system/deduplication.service /tmpRoot/lib/systemd/system/multi-user.target.wants/deduplication.service
+  ln -vsf /usr/lib/systemd/system/deduplicationhdd.service /tmpRoot/lib/systemd/system/multi-user.target.wants/deduplicationhdd.service
 elif [ "${1}" = "uninstall" ]; then
-  echo "Installing addon deduplication - ${1}"
+  echo "Installing addon deduplicationhdd - ${1}"
 
-  rm -f "/tmpRoot/lib/systemd/system/multi-user.target.wants/deduplication.service"
-  rm -f "/tmpRoot/usr/lib/systemd/system/deduplication.service"
+  rm -f "/tmpRoot/lib/systemd/system/multi-user.target.wants/deduplicationhdd.service"
+  rm -f "/tmpRoot/usr/lib/systemd/system/deduplicationhdd.service"
 
   [ ! -f "/tmpRoot/usr/arc/revert.sh" ] && echo '#!/usr/bin/env bash' >/tmpRoot/usr/arc/revert.sh && chmod +x /tmpRoot/usr/arc/revert.sh
   echo "/usr/bin/deduplication.sh --restore" >>/tmpRoot/usr/arc/revert.sh
